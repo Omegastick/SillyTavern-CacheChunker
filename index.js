529c3bbbd75fd6e292d16a1dd9ff094f35a2332f
omegastick <isaac@poulton.dev>
2024-03-11 22:40:04 +0000

    Use guesstimate as a starting point for the search

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { saveSettingsDebounced } from '../../../../script.js';
 import { extension_settings, renderExtensionTemplate } from '../../../extensions.js';
-import { getTokenCount } from '../../../tokenizers.js';
+import { getTokenCount, guesstimate } from '../../../tokenizers.js';
 
 const MODULE_NAME = 'CacheChunker';
 
@@ -17,6 +17,10 @@ const settings = {
 function trimContext(chat) {
     console.debug('Trimming context, message count before:', chat.length);
 
+    const startingPoint = getStartingPoint(chat, settings.maxMessageHistoryContext, settings.chunkSize);
+    console.debug('Starting point:', startingPoint);
+    chat.splice(0, startingPoint);
+
     while (getTokenCount(renderChat(chat)) > settings.maxMessageHistoryContext) {
         chat.splice(0, settings.chunkSize);
     }
@@ -28,6 +32,30 @@ function renderChat(chat) {
     return chat.map((message) => {
         return message.mes;
     }).join('\n');
+}
+
+/**
+ * Uses guesstimate to get a starting point for the chunking process.
+ * @param {object[]} chat Array of chat messages
+ * @param {number} maxTokens Maximum number of tokens to allow in the chat array
+ * @param {number} chunkSize Number of messages to remove at a time
+ * @param {number} tolerance Amount added to the estimated starting point as a percentage of the total message count
+ * @returns {number} Index of the starting point for the chunking process
+ */
+function getStartingPoint(chat, maxTokens, chunkSize, tolerance = 0.1) {
+    const totalTokens = guesstimate(renderChat(chat));
+    const target = maxTokens + (maxTokens * tolerance);
+
+    if (totalTokens <= target) {
+        return 0;
+    }
+
+    const targetAsPercentage = target / totalTokens;
+    const targetWithTolerance = targetAsPercentage + tolerance;
+    const index = Math.floor(chat.length * targetWithTolerance);
+    const chunkedIndex = index - (index % chunkSize);
+
+    return chunkedIndex;
 }
 
 window['CacheChunker_trimContext'] = trimContext;
